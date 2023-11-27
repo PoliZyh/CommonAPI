9f8fc6b64d0d9838a34b453320e605c9975a76da
PoliZyh <272798802@qq.com>
2023-11-27 14:25:42 +0800

    feat: 路由文件自动加载

--- src/app/index.js
+++ src/app/index.js
@@ -3,13 +3,12 @@ const { koaBody } = require('koa-body');
 
 const errHandler = require('./errHandler')
 
-const userRouter = require('../router/user.route');
-
+const router = require('../router');
 
 const app = new Koa();
 
 app.use(koaBody());
-app.use(userRouter.routes())
+app.use(router.routes()).use(router.allowedMethods());
 
 // 统一错误处理
 app.on('error', errHandler)
---
+++ src/controller/goods.comtroller.js
@@ -0,0 +1,9 @@
+
+class GoodsController {
+    async upload(ctx, next) {
+        ctx.body = '图片上传成功'
+    }
+}
+
+
+module.exports = new GoodsController()
